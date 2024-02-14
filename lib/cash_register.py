60c0c4eb235bf3d2d8d6df88940d157471728153
Tony Eder <tonyeder11@gmail.com>
2024-02-14 09:51:27 -0800

    baby steps

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+
+  def __init__(self, discount=0, total=0, items=[]):
+      self.discount = discount
+      self.total = total
+      self.items = items
+
+  @property
+  def discount(self):
+      return self._discount
+  
+  @discount.setter
+  def discount(self, discount):
+      self._discount = discount
+  
+  @property
+  def total(self):
+      return self._total
+  
+  @total.setter
+  def total(self, total):
+      self._total = total  
+
+  @property
+  def items(self):
+      return self._items
+  
+  @items.setter
+  def items(self, items):
+      self._items = items
+
