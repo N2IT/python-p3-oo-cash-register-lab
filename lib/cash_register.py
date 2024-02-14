9e4262e9fd344e0b9275698391f3d1fc3d592e83
Tony Eder <tonyeder11@gmail.com>
2024-02-14 14:49:01 -0800

    add items all cleared

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -2,35 +2,35 @@
 
 class CashRegister:
 
-  def __init__(self, discount=0, total=0, items=[]):
+  def __init__(self, discount=0):
       self.discount = discount
-      self.total = total
-      self.items = items
+      self.total = 0
+      self.items = []
       # self.quantity = quantity
 
-  @property
-  def discount(self):
-      return self._discount
+  # @property
+  # def discount(self):
+  #     return self._discount
   
-  @discount.setter
-  def discount(self, discount):
-      self._discount = discount
+  # @discount.setter
+  # def discount(self, discount):
+  #     self._discount = discount
   
-  @property
-  def total(self):
-      return self._total
+  # @property
+  # def total(self):
+  #     return self._total
   
-  @total.setter
-  def total(self, total):
-      self._total = total  
+  # @total.setter
+  # def total(self, total):
+  #     self._total = total  
 
-  @property
-  def items(self):
-      return self._items
+  # @property
+  # def items(self):
+  #     return self._items
   
-  @items.setter
-  def items(self, items):
-      self._items = items
+  # @items.setter
+  # def items(self, items):
+  #     self._items = items
 
   # @property
   # def quantity(self):
@@ -42,10 +42,26 @@ class CashRegister:
 
   def add_item(self, title, price, quantity=0):
       # breakpoint()
+      # self.items.append(title)
       if quantity > 0:
-        self.total += (price * quantity) 
+        x = range(quantity)
+        breakpoint()
+        for q in x:
+          self.items.append(title)
+        self.total += (price * quantity)
         return self.total
       else:
+        self.items.append(title)
         self.total += price
         return self.total
+      return self.items
       
+  def apply_discount(self):
+    # breakpoint()
+    self.total = int(self.total - (self.discount * 10))
+    # print('There is not discount to apply.')
+    if self.discount > 0:
+      print(f'After the discount, the total comes to ${self.total}.')
+      return self.total
+    else:
+      print('There is no discount to apply.')
